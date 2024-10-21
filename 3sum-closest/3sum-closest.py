1382f47311ee6d83792fcb7f36edf2d66d711230
AnuragMishra1712 <90804466+AnuragMishra1712@users.noreply.github.com>
2024-10-21 12:14:35 -0400

    Time: 454 ms, Memory: 11.7 MB - LeetHub

--- 3sum-closest/3sum-closest.py
+++ 3sum-closest/3sum-closest.py
@@ -5,52 +5,28 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        n = len(nums)
+    
         nums.sort()
         closest_sum = float('inf')
-
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
+    
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-
-            lo, hi = i + 1, n - 1
-            while lo < hi:
-                cur_sum = nums[i] + nums[lo] + nums[hi]
-
-                if abs(cur_sum - target) < abs(closest_sum - target):
+            
+            l,r = i+1,len(nums)-1
+            
+            while l<r:
+                cur_sum = nums[i] + nums[l] + nums[r]
+                if abs(cur_sum -target) < abs(closest_sum - target):
                     closest_sum = cur_sum
-
+                
                 if cur_sum == target:
                     return cur_sum
-                elif cur_sum < target:
-                    lo += 1
+                elif cur_sum<target:
+                    l+=1
                 else:
-                    hi -= 1
-
-        return closest_sum
-        # Time: O(n^2)
-        # Space: O(n)
-#         nums.sort()
-#         closest_sum = float('inf')
-    
-#         for i in range(len(nums)):
-#             if i>0 and nums[i] == nums[i-1]:
-#                 continue
-            
-#             l,r = i+1,len(nums)-1
-            
-#             while l<r:
-#                 cur_sum = nums[i] + nums[l] + nums[r]
-#                 if abs(cur_sum -target) < abs(closest_sum - target):
-#                     closest_sum = cur_sum
+                    r-=1
                 
-#                 if cur_sum == target:
-#                     return cur_sum
-#                 elif cur_sum<target:
-#                     l+=1
-#                 else:
-#                     r-=1
-                
-#         return cur_sum        
+        return closest_sum       
     
     
